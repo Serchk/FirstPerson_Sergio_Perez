29e65cdfbae5c5e7fbe62f2a5df9cd0f1463dd4a
Serchk <sergioperezalcal@gmail.com>
2024-10-21 20:28:07 +0200

    tercera persona mal

---
+++ Assets/Scripts/ThirdPerson.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPerson : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float smoothTime;
+    CharacterController controller;
+
+    private float velocidadRotacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverTRotar();
+    }
+    void MoverTRotar()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+
+        Vector2 input = new Vector2(h, v).normalized;
+
+        //Calculo el ángulo al que tengo que rotarme en función de los inputs y cámara
+        
+
+        if (input.magnitude > 0)
+        {
+
+            float angulo = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.rotation.eulerAngles.y;
+
+            float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, angulo, ref velocidadMovimiento, smoothTime);
+
+            transform.eulerAngles = new Vector3(0, anguloSuave, 0);
+
+            //Mi movimiento tambian ha quedado rotado en la base
+            Vector3 movimiento = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
+
+            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        }
+    }
+}
