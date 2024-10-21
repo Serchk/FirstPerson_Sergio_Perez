956101a97575c980435950241640db068293b8b1
Serchk <sergioperezalcal@gmail.com>
2024-10-21 19:40:57 +0200

    first person solucionado a medias

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -21,15 +21,30 @@ public class FirstPerson : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        
-        Vector3 movimiento = new Vector3(h, 0, v).normalized;
 
-        float anguloRotacion = Mathf.Atan2(movimiento.x, movimiento.z) * Mathf.Rad2Deg + Camera.main.transform.rotation.eulerAngles.y;
-        if(movimiento.magnitude > 0)
+        
+        Vector2 input = new Vector2(h, v).normalized;
+
+        //Calculo el ángulo al que tengo que rotarme en función de los inputs y cámara
+        float angulo = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.rotation.eulerAngles.y;
+
+            transform.eulerAngles = new Vector3(0, angulo, 0);
+
+        if (input.magnitude > 0)
         {
-            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+            //Mi movimiento tambian ha quedado rotado en la base
+            Vector3 movimiento = Quaternion.Euler(0,angulo, 0) * Vector3.forward;
             controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
         }
-       
+
+        //Vector3 movimiento = new Vector3(h, 0, v).normalized;
+
+        //float anguloRotacion = Mathf.Atan2(movimiento.x, movimiento.z) * Mathf.Rad2Deg + Camera.main.transform.rotation.eulerAngles.y;
+        //if (movimiento.magnitude > 0)
+        //{
+        //    transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+        //    controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        //}
+
     }
 }
