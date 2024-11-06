0089e40628717c9024a358f6e8e560f6a3969280
Serchk <sergioperezalcal@gmail.com>
2024-11-06 17:15:30 +0100

    particulas

---
+++ Assets/Scripts/Arma/ArmaSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Arma")]
+public class ArmaSO : ScriptableObject
+{
+    //DATOS
+    public int balasCargador;
+    public int balasBolsa;
+    public float cadencia;
+    public float distanciaAtaque;
+    public float danhoAtaque;
+}
